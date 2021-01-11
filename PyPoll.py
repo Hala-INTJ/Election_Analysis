70c81e3065147f25358244f1b028e1efe8af5fb2
Hala Jouaneh <hala.jouaneh@gmail.com>
2021-01-11 15:20:59 -0500

    coding

--- PyPoll.py
+++ PyPoll.py
@@ -5,22 +5,20 @@
 # # Print the present time.
 # print("The time right now is ", now)
 
-# Import csv 
-import csv
-dir(csv)
-
-
-
-
 # The data we need to retrieve
-# Resources/election_results.csv
+# Assign a variable for the file to load and the path.
+file_to_load = 'Resources/election_results.csv'
+# Open the election results and read the file.
+# election_data = open(file_to_load, 'r')
+with open(file_to_load) as election_data:
 
-
-
-
+# To do: perform analysis.
 
 # 1. The total number of votes cast
 # 2. A complete list of candidates who received votes
 # 3. The percentage of votes each candidate won
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on popular vote
+
+# Close the file.
+#election_data.close()
