5fd78546cae577dc0c19098c177e78f9d1ee08d9
Hala Jouaneh <hala.jouaneh@gmail.com>
2021-01-11 00:10:15 -0500

    coding

---
+++ Python_practice.py
@@ -0,0 +1,45 @@
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+# # Retreive each dictionary in the list
+
+# # Option 1
+# for item in voting_data:
+#     print(item)
+
+# # Option 2
+# for i in range(len(voting_data)):
+#     print (voting_data[i])
+
+# # Retreive only the values of each dictionary
+# for item in voting_data:
+#     print(item.values())
+
+# for item in voting_data:
+#     for value in item.values():
+#         print(value)
+
+# # Retreive the key-value pairs of each dictionary
+# for item in voting_data:
+#     for key, value in item.items():
+#         print(key,value)
+
+# # Retreive only the county name from each dictionary
+# for item in voting_data:
+#     print(item['county']) 
+
+# # Print each county and registered voter 
+# counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+
+# # Option 1
+# for county,voters in counties_dict.items():
+#     print(county + " county has " + str(voters) + " registered voters.")
+
+# # Option 2
+# for county,voters in counties_dict.items():
+#     print(f"{county} county has {voters:,} registered voters.")
+
+# Print each county and registered voter from the voting_data
+for item in voting_data:
+    print(f"{item['county']} county has {item['registered_voters']:,} registered voters.")
